d24b2f79b15982f08b37caa5b336a470810a19d7
frag223 <m.riquelme223@gmail.com>
2025-08-10 11:54:25 +1000

    boilerplate

---
+++ internal/database/database.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"database/sql"
+	_ "embed"
+	"path"
+
+	_ "modernc.org/sqlite"
+)
+
+//go:embed schemas/schema.sql
+var ddl string
+
+const dbName = "watchtower.db"
+
+type Migrator struct {
+	db *sql.DB
+}
+
+func NewMigrator(filePath string) (*Migrator, error) {
+	dbFile := path.Join(filePath, dbName)
+	db, err := sql.Open("sqlite", dbFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Migrator{db: db}, nil
+}
+
+func (m *Migrator) Init() error {
+	_, err := m.db.Exec(ddl)
+	return err
+}
