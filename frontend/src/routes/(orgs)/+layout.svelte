bab6888b893b48520983234fd27076917ed9b85c
frag223 <m.riquelme223@gmail.com>
2025-08-14 21:29:26 +1000

    wip

---
+++ frontend/src/routes/(orgs)/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { type LayoutProps } from "./$types";
+
+	let { data, children }: LayoutProps = $props();
+</script>
+
+<div class="flex">
+	<aside class="flex min-h-screen min-w-24 flex-col bg-muted p-2 shadow-sm">
+		<span class="text-sm">{data?.organisation?.friendly_name}</span>
+		<div class="flex-1 bg-pink-400">
+			<p>products</p>
+			<p>organisations</p>
+		</div>
+		<p>settings</p>
+	</aside>
+	{@render children?.()}
+</div>
