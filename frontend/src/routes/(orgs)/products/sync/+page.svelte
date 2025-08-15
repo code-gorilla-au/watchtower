1357b27cd2debc7f4f95767c9a4ec93994bedc46
frag223 <m.riquelme223@gmail.com>
2025-08-15 15:02:45 +1000

    adding sync page

---
+++ frontend/src/routes/(orgs)/products/sync/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { PageTitle } from "$components/page_title";
+</script>
+
+<div class="w-full p-2">
+	<PageTitle title="Sync Product" subtitle="Sync pull request from GitHub" />
+</div>
