2b4023f91fdc145dea3f7751d54b21c89847442d
frag223 <m.riquelme223@gmail.com>
2025-08-18 07:32:54 +1000

    fixing nav

--- frontend/src/routes/(orgs)/+layout.svelte
+++ frontend/src/routes/(orgs)/+layout.svelte
@@ -1,12 +1,29 @@
 <script lang="ts">
 	import { type LayoutProps } from "./$types";
-	import { Settings, Package, Castle, LayoutDashboard } from "@lucide/svelte";
+	import {
+		Settings,
+		Package,
+		Castle,
+		LayoutDashboard,
+		ChevronLeft,
+		ChevronRight
+	} from "@lucide/svelte";
 	import { cn } from "$lib/utils";
 	import { NavItem } from "$components/nav/index.js";
+	import { orgSvc } from "$lib/watchtower";
+	import { Button } from "$components/ui/button";
+	import { Separator } from "$components/ui/separator";
 
 	let { children }: LayoutProps = $props();
+
+	const organisation = $derived(orgSvc.defaultOrg);
+
 	let expand = $state(true);
 	let expandedStyle = $derived(expand ? "min-w-40" : "w-14");
+	function toggleExpand(e: Event) {
+		e.preventDefault();
+		expand = !expand;
+	}
 </script>
 
 <div class="flex">
@@ -17,6 +34,22 @@
 		)}
 	>
 		<div class="flex flex-1 flex-col gap-2">
+			<div class="py-2">
+				{#if expand}
+					<span class="font-bold">
+						{organisation?.friendly_name}
+					</span>
+				{/if}
+
+				<Button onclick={toggleExpand} size="sm" variant="ghost" class="ml-auto">
+					{#if expand}
+						<ChevronLeft />
+					{:else}
+						<ChevronRight />
+					{/if}
+				</Button>
+			</div>
+			<Separator class="mb-2" />
 			<NavItem to="/" {expand} label="Dashboard">
 				{#snippet icon()}
 					<LayoutDashboard size={24} />
