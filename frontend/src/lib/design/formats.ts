07a5a136fbd8d4ea9e06cbb94689692f779ac43f
frag223 <m.riquelme223@gmail.com>
2025-08-21 20:49:32 +1000

    adding docs

--- frontend/src/lib/design/formats.ts
+++ frontend/src/lib/design/formats.ts
@@ -1,12 +1,14 @@
 import { formatDistanceToNow } from "date-fns";
 
+/**
+ * Formats the given date to display the relative time (distance to now).
+ */
 export function formatDate(date: Date) {
 	return formatDistanceToNow(date);
 }
 
 /**
- *
- * @param str
+ * Truncates the provided string to a maximum of 100 characters and appends an ellipsis ("...") at the end.
  */
 export function truncate(str: string) {
 	const sub = str.substring(0, 100);
