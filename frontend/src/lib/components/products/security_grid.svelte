7295d9f6ccf92de022a36e5af71b6a01623e7ce5
frag223 <m.riquelme223@gmail.com>
2025-08-22 08:19:24 +1000

    adding get security components

---
+++ frontend/src/lib/components/products/security_grid.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { SecurityCard } from "$components/products/index";
+	import { EmptySlate } from "$components/empty_slate";
+	import { Grid } from "$components/grid";
+	import { watchtower } from "$lib/wailsjs/go/models";
+
+	type Props = {
+		securities: watchtower.SecurityDTO[];
+	};
+	let { securities }: Props = $props();
+</script>
+
+{#if securities.length === 0}
+	<EmptySlate title="No security vulnerabilities"></EmptySlate>
+{:else}
+	<Grid>
+		{#each securities as security (security.id)}
+			<SecurityCard {security} />
+		{/each}
+	</Grid>
+{/if}
