d109bb8b94cf1ec9d6193ff8cc15f0360e8034cf
frag223 <m.riquelme223@gmail.com>
2025-08-22 08:30:01 +1000

    adding to dashboard

--- frontend/src/routes/(orgs)/dashboard/+page.svelte
+++ frontend/src/routes/(orgs)/dashboard/+page.svelte
@@ -5,11 +5,13 @@
 	import { onDestroy, onMount } from "svelte";
 	import { TIME_TWO_MINUTES } from "$lib/watchtower/types";
 	import { invalidateAll } from "$app/navigation";
+	import { SecurityGrid } from "$components/products/index.js";
 
 	let { data }: PageProps = $props();
 	let org = $derived(data.organisation);
 	let products = $derived(data.products);
 	let prs = $derived(data.prs);
+	let securities = $derived(data.securities);
 
 	let intervalPoll: number;
 	onMount(() => {
@@ -25,6 +27,11 @@
 
 <div class="page-container">
 	<PageTitle title="Dashboard" subtitle={org?.friendly_name} />
+
+	<div class="my-4">
+		<h3 class="text-xl text-muted-foreground">Security Vulnerability</h3>
+	</div>
+	<SecurityGrid {securities} />
 
 	<div class="my-4">
 		<h3 class="text-xl text-muted-foreground">Pull Requests</h3>
--- frontend/src/routes/(orgs)/dashboard/+page.ts
+++ frontend/src/routes/(orgs)/dashboard/+page.ts
@@ -5,10 +5,12 @@ export const load: PageLoad = async () => {
 	await orgSvc.getDefault();
 	const products = await productSvc.getAllByOrgId(orgSvc.defaultOrg?.id as number);
 	const prs = await productSvc.getPullRequestsByOrganisation(orgSvc.defaultOrg?.id as number);
+	const securities = await productSvc.getSecurityByOrganisation(orgSvc.defaultOrg?.id as number);
 
 	return {
 		organisation: orgSvc.defaultOrg,
 		products,
-		prs
+		prs,
+		securities
 	};
 };
