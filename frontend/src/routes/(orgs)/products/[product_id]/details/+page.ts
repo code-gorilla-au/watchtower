3107ac735b4ab94cf6001323959a7ec73e0daa48
frag223 <m.riquelme223@gmail.com>
2025-08-22 08:33:07 +1000

    adding grid for securities

--- frontend/src/routes/(orgs)/products/[product_id]/details/+page.ts
+++ frontend/src/routes/(orgs)/products/[product_id]/details/+page.ts
@@ -5,6 +5,7 @@ export const load: PageLoad = async ({ params }) => {
 	const product = await productSvc.getById(Number(params.product_id));
 	const repos = await productSvc.getProductRepos(product.id);
 	const prs = await productSvc.getOpenPrsByProduct(product.id);
+	const securities = await productSvc.getSecurityByProduct(product.id);
 
-	return { product, repos, prs };
+	return { product, repos, prs, securities };
 };
