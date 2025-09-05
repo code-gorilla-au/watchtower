e9b88a3ed2618f4a74d045acf02b1e62fbdc9c97
frag223 <m.riquelme223@gmail.com>
2025-09-05 22:36:39 +1000

    wip adding filter

--- frontend/src/lib/hooks/filters.svelte.test.ts
+++ frontend/src/lib/hooks/filters.svelte.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from "vitest";
 import { SimpleFilter } from "$lib/hooks/filters.svelte";
+import { flushSync } from "svelte";
 
 describe("filters", () => {
 	describe("simple filters", () => {
@@ -8,9 +9,40 @@ describe("filters", () => {
 
 			expect(filter.data).toEqual([]);
 		});
+
 		it("should return an array with data", () => {
 			const filter = new SimpleFilter([1, 2, 3]);
 			expect(filter.data).toEqual([1, 2, 3]);
+		});
+
+		it("it should apply filter if provided", () => {
+			const filter = new SimpleFilter([1, 2, 3], (item) => {
+				return item > 1;
+			});
+
+			expect(filter.data).toEqual([2, 3]);
+		});
+
+		it("clearing the filter should return original data", () => {
+			const filter = new SimpleFilter([1, 2, 3], (item) => {
+				return item > 1;
+			});
+			expect(filter.data).toEqual([2, 3]);
+
+			filter.clear();
+			expect(filter.data).toEqual([1, 2, 3]);
+		});
+
+		it("it should apply filter if provided", () => {
+			const filter = new SimpleFilter([1, 2, 3]);
+			expect(filter.data).toEqual([1, 2, 3]);
+
+			filter.filterBy((item) => {
+				return item > 1;
+			});
+			flushSync();
+
+			expect(filter.data).toEqual([2, 3]);
 		});
 	});
 });
--- frontend/src/lib/hooks/filters.svelte.ts
+++ frontend/src/lib/hooks/filters.svelte.ts
@@ -1,6 +1,7 @@
 export type FilterFunc<T> = (item: T) => boolean;
 
 export interface FilterState<T> {
+	initData: T[];
 	data: T[];
 	filterFn?: FilterFunc<T>;
 }
@@ -11,8 +12,32 @@ export class SimpleFilter<T> {
 	readonly data: T[];
 
 	constructor(data: T[], filterFn?: FilterFunc<T>) {
-		this.#internal = $state({ data, filterFn });
-
+		this.#internal = $state({ data, initData: data, filterFn });
 		this.data = $derived(this.#internal.data);
+
+		this.applyFilter();
+
+		$effect(() => {
+			this.applyFilter();
+		});
+	}
+
+	clear() {
+		this.#internal.filterFn = undefined;
+		this.applyFilter();
+	}
+
+	filterBy(filterFn: FilterFunc<T>) {
+		this.#internal.filterFn = filterFn;
+		this.applyFilter();
+	}
+
+	private applyFilter() {
+		if (this.#internal.filterFn) {
+			this.#internal.data = this.#internal.data.filter(this.#internal.filterFn);
+			return;
+		}
+
+		this.#internal.data.splice(0, this.#internal.data.length, ...this.#internal.initData);
 	}
 }
