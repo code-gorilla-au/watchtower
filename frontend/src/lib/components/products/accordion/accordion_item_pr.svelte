930cca4438e09cee82f13307c8a5f0b1d7de2361
frag223 <m.riquelme223@gmail.com>
2025-12-05 11:02:24 +1100

    format

--- frontend/src/lib/components/products/accordion/accordion_item_pr.svelte
+++ frontend/src/lib/components/products/accordion/accordion_item_pr.svelte
@@ -3,11 +3,11 @@
 	import { GridHeader } from "$components/grid";
 	import { PRGrid } from "$components/products";
 	import { watchtower } from "$lib/wailsjs/go/models";
-    import {type FilterTagValue, TagsFilter} from "$lib/hooks/filters.svelte";
+	import { type FilterTagValue, TagsFilter } from "$lib/hooks/filters.svelte";
 
 	type Props = {
 		prs: watchtower.PullRequestDTO[];
-        fieldTag?: FilterTagValue<watchtower.PullRequestDTO>;
+		fieldTag?: FilterTagValue<watchtower.PullRequestDTO>;
 	};
 
 	let { prs, fieldTag = "tag" }: Props = $props();
