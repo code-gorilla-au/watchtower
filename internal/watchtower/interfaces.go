344b7ccdc632dcdfdd6dadf7b38142fefe7140d4
Mauricio Riquelme <m.riquelme223@gmail.com>
2026-01-16 13:48:00 +1100

    feat: improving nav and removing dead code (#47)
    - fix: remove sql queries
    - feat: improve ux for org switching

--- internal/watchtower/interfaces.go
+++ internal/watchtower/interfaces.go
@@ -1,9 +1,6 @@
 package watchtower
 
 import (
-	"context"
-	"watchtower/internal/database"
-
 	"watchtower/internal/github"
 )
 
@@ -13,10 +10,3 @@ type ghClient interface {
 	SearchRepos(owner string, topic string, token string) (github.QuerySearch[github.Repository], error)
 	GetRepoDetails(owner string, repo string, token string) (github.QueryRepository, error)
 }
-
-type InsightsStore interface {
-	GetPullRequestInsights(ctx context.Context, arg database.GetPullRequestInsightsParams) (database.GetPullRequestInsightsRow, error)
-	GetSecuritiesInsights(ctx context.Context, arg database.GetSecuritiesInsightsParams) (database.GetSecuritiesInsightsRow, error)
-}
-
-var _ InsightsStore = (*database.Queries)(nil)
